f2f1debfbcf774a574df5905b4cb12f3768162ec
Tristan <tristan.michel@etu.umpc.fr>
2019-12-26 18:20:16 +0100

    Correction detecteur ultrason

--- Eleguide/Leáon 4 - Eviter les obstacles/Servo_debug/Servo_debug.ino
+++ Eleguide/Leáon 4 - Eviter les obstacles/Servo_debug/Servo_debug.ino
@@ -4,7 +4,7 @@ Servo myservo;
 
 void setup(){
   myservo.attach(3);
-  myservo.write(90);// move servos to center position -> 90°
+  myservo.write(170);// move servos to center position -> 90°
 } 
 void loop(){
 
--- Robot_car/Robot_car_detector/Robot_car_detector.ino
+++ Robot_car/Robot_car_detector/Robot_car_detector.ino
@@ -1,25 +1,32 @@
+// servo left max = 170 deg
+// servo right max = 0 deg
+// servo middle = 85 deg
+
 #include <Servo.h>
 
 #define Echo A4
 #define Trig A5
+#define Servo_center_pos 85
+
 
 Servo servo;
 float dist_l, dist_c, dist_r;
+int ecart = 90;
 
 void servo_left(int d) {
-  servo.write(90);
+  servo.write(Servo_center_pos + ecart);
   Serial.println("Servo Left");
   delay(d);
 }
 
 void servo_center(int d) {
-  servo.write(45);
+  servo.write(Servo_center_pos);
   Serial.println("Servo Center");
   delay(d);
 }
 
 void servo_right(int d) {
-  servo.write(0);
+  servo.write(Servo_center_pos - ecart);
   Serial.println("Servo Right");
   delay(d);
 }
@@ -38,32 +45,40 @@ float mesure_distance() {
 } 
 
 void set_distances() {
-  int t = 200;
-  servo_left(t);
-  dist_l = mesure_distance();
-  delay(t);
-  servo_center(t);
-  dist_c = mesure_distance();
-  delay(t);
-  servo_right(t);
-  dist_r = mesure_distance();
-  delay(t);
-  Serial.print("Distance left = ");
-  Serial.print(dist_l);
-  Serial.print(", center = ");
-  Serial.print(dist_c);
-  Serial.print(", right = ");
-  Serial.println(dist_r);
+    int t = 6*ecart;
+    servo_left(t);
+    dist_l = mesure_distance();
+    delay(t);
+    servo_center(t);
+    dist_c = mesure_distance();
+    delay(t);
+    servo_right(t);
+    dist_r = mesure_distance();
+    delay(t);
+    Serial.print("Distance left = ");
+    Serial.print(dist_l);
+    Serial.print(", center = ");
+    Serial.print(dist_c);
+    Serial.print(", right = ");
+    Serial.println(dist_r);
 }
 
 
 void setup() {
   Serial.begin(9600);
   servo.attach(3);
-  servo_center(100);
+  pinMode(Echo, INPUT);
+  pinMode(Trig, OUTPUT);
+//  pinMode(IN1, OUTPUT);
+//  pinMode(IN2, OUTPUT);
+//  pinMode(IN3, OUTPUT);
+//  pinMode(IN4, OUTPUT);
+//  pinMode(ENA, OUTPUT);
+//  pinMode(ENB, OUTPUT);
 }
 
 void loop() {
   set_distances();
 //  delay(1000);
+  ecart = (ecart < 20) ? ecart : ecart / 1.5;
 }
