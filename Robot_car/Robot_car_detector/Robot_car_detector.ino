8ca83f148cc1d43dbda7a72c7c1dd340145ac933
Tristan <tristan.michel@etu.umpc.fr>
2019-12-28 00:21:00 +0100

    New detection function

--- Robot_car/Robot_car_detector/Robot_car_detector.ino
+++ Robot_car/Robot_car_detector/Robot_car_detector.ino
@@ -7,11 +7,11 @@
 #define Echo A4
 #define Trig A5
 #define Servo_center_pos 85
-#define nb_mesure 5
+#define nb_mesure 3
 
 Servo servo;
-float dist_l, dist_c, dist_r;
-int ecart = 30;
+float dist_l, dist_c, dist_r, dist;
+int deg, ecart = 75, dist_min = 8, dist_max = 12;
 
 
 void servo_left(int d) {
@@ -30,6 +30,14 @@ void servo_right(int d) {
   servo.write(Servo_center_pos - ecart);
   Serial.println("Servo Right");
   delay(d);
+}
+
+void servo_at(int deg, int dt) {
+  servo.write(deg);
+  Serial.print("Servo @ ");
+  Serial.print(deg);
+  Serial.println(" degres");
+  delay(dt);
 }
 
 float mesure_distance() {
@@ -67,35 +75,78 @@ float get_median(float tab[]){
 }
 
 void set_distances() {
-    int t = 5*ecart;
-    float tab[nb_mesure];
-    servo_left(t);
-    delay(t);
-    for (int i = 0; i < nb_mesure; i++){
-      tab[i] = mesure_distance();
-      delay(5);
+  int t = 15*ecart;
+  float tab[nb_mesure];
+  servo_left(t);
+  for (int i = 0; i < nb_mesure; i++){
+    tab[i] = mesure_distance();
+    delay(5);
+  }
+  dist_l = get_median(tab);
+  servo_center(t);
+  for (int i = 0; i < nb_mesure; i++){
+    tab[i] = mesure_distance();
+    delay(5);
+  }
+  dist_c = get_median(tab);
+  servo_right(t);
+  for (int i = 0; i < nb_mesure; i++){
+    tab[i] = mesure_distance();
+    delay(5);
+  }
+  dist_r = get_median(tab);
+  Serial.print("Distance left = ");
+  Serial.print(dist_l);
+  Serial.print(", center = ");
+  Serial.print(dist_c);
+  Serial.print(", right = ");
+  Serial.println(dist_r);
+}
+
+void set_distances2() {
+  int dt = 150;
+  float tab[nb_mesure];
+  dist = 3000.0;
+  for (int i = Servo_center_pos - ecart; i <= Servo_center_pos + ecart; i += 25){
+    servo_at(i,dt);
+    
+    for (int j = 0; j < nb_mesure; j++){
+      tab[j] = mesure_distance();
+      delay(2);
     }
-    dist_l = get_median(tab);
-    servo_center(t);
-    delay(t);
-    for (int i = 0; i < nb_mesure; i++){
-      tab[i] = mesure_distance();
-      delay(5);
+
+    if (dist >= get_median(tab)){
+      dist = get_median(tab);
+      deg = i;
     }
-    dist_c = get_median(tab);
-    servo_right(t);
-    delay(t);
-    for (int i = 0; i < nb_mesure; i++){
-      tab[i] = mesure_distance();
-      delay(5);
-    }
-    dist_r = get_median(tab);
-    Serial.print("Distance left = ");
-    Serial.print(dist_l);
-    Serial.print(", center = ");
-    Serial.print(dist_c);
-    Serial.print(", right = ");
-    Serial.println(dist_r);
+  }
+  Serial.print("Objet @ ");
+  Serial.print(dist);
+  Serial.print(" cm & @ ");
+  Serial.print(deg);
+  Serial.println(" degres");
+}
+
+void face_object() {
+  set_distances2();
+  
+  servo_at(deg,0);
+
+  if (deg - Servo_center_pos >= 0){
+    Serial.print(deg - Servo_center_pos);
+    Serial.println(" degres left ------------------------");
+  } else {
+    Serial.print(Servo_center_pos - deg);
+    Serial.println(" degres right -----------------------");
+  }
+
+  if (dist > dist_max) {
+    Serial.print("moving forward (cm) ");
+    Serial.println(dist-dist_max);
+  } else if (dist < dist_min) {
+    Serial.print("moving backward (cm) ");
+    Serial.println(dist-dist_min);
+  }
 }
 
 
@@ -110,10 +161,11 @@ void setup() {
 //  pinMode(IN4, OUTPUT);
 //  pinMode(ENA, OUTPUT);
 //  pinMode(ENB, OUTPUT);
+  servo_center(50);
 }
 
 void loop() {
-  set_distances();
-  delay(1000);
+  face_object();
+  delay(10000);
 //  ecart = (ecart < 20) ? ecart : ecart / 1.5;
 }
